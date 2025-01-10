98b2f98ac03f5eac58f52ba883fdf1ca611a8b03
rlsj0 <rodrileja@gmail.com>
2025-01-10 11:58:27 +0100

    reformat Product class

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,23 +1,25 @@
 namespace Models;
 
-public abstract class Producto {
-	public int Id {get; private set;}
-	public static int nextId {get; set;} = 0;
-	public decimal Precio {get; set}
-	public int CantidadStock {get; set;}
-	public int NumeroCompras {get; set;}
-	public bool EsComercioJusto {get; private set;}
+public abstract class Producto
+{
+    public int Id { get; private set; }
+    public static int nextId { get; set; } = 0;
+    public decimal Precio { get; set; }
+    public int CantidadStock { get; set; }
+    public int NumeroCompras { get; set; }
+    public bool EsComercioJusto { get; private set; }
 
-	// Constructor para crear un nuevo producto
-	public class Producto (decimal precio, int cantidadStock, bool esComercioJusto) {
-		Id = ++nextId;
-		Precio = precio;
-		CantidadStock = cantidadStock;
-		NumeroCompras = 0;
-		EsComercioJusto = esComercioJusto;
-	}
-	
-	public ModificarStock();
+    // Constructor para crear un nuevo producto
+    public Producto(decimal precio, int cantidadStock, bool esComercioJusto)
+    {
+        Id = ++nextId;
+        Precio = precio;
+        CantidadStock = cantidadStock;
+        NumeroCompras = 0;
+        EsComercioJusto = esComercioJusto;
+    }
 
-	public MostrarInformacion();
+    public abstract void ModificarStock();
+
+    public abstract void MostrarInformacion();
 }
