0e816843af965f62079ffb119dc4851743a7761f
rlsj0 <rodrileja@gmail.com>
2025-01-10 12:54:52 +0100

    modify Program.cs for testing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+Cafe BrasilSolo = new Cafe(1.80m, 20, true, "Brasil", "Solo");
+Cafe BrasilLargo = new Cafe(2.00m, 15, true, "Brasil", "Largo");
+Cafe JamaicaLargo = new Cafe(4, 2.00m, 15, 4, true, "Brasil", "Largo");
+Cafe BrasilLeche = new Cafe(2.50m, 15, true, "Brasil", "Con leche");
+
+BrasilSolo.MostrarInformacion();
+
+BrasilLargo.RestarStock(3);
+BrasilLargo.MostrarInformacion();
+
+JamaicaLargo.MostrarInformacion();
+BrasilLeche.MostrarInformacion();
